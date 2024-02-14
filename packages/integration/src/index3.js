dfb792f594ef565ba5bc77d51d630c274b219bee
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2024-02-14 19:34:18 +0530

    implement UI design login

--- packages/integration/src/index3.js
+++ packages/integration/src/index3.js
@@ -1,5 +1,78 @@
-import { incrementCounterHandler } from "../../package-1/src/index1";
+import {
+  h,
+  init,
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule,
+  toVNode
+} from "snabbdom";
 
-const addButton = document.getElementById("addButton");
+const patch = init([
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule
+]);
 
-addButton.addEventListener("click", incrementCounterHandler);
+let state = { count: 1 };
+
+// function updateState(state) {
+//   return { ...state, count: state.count + 1 }; // Create a new state object with updated count
+// }
+
+function increment() {
+  const value = { count: state.count + 1 };
+  return value;
+}
+
+function updateView(value) {
+  let oldView = template(state);
+  let newView = template(value);
+
+  console.log(oldView, newView);
+  render(oldView, newView);
+  state = value;
+
+  //should increment state
+
+  //should update the view - call render function
+}
+
+function template(state) {
+  const view = h("div#app", [
+    h("h1", state.count),
+    h(
+      "button",
+      {
+        on: {
+          click: () => {
+            const value = increment();
+            updateView(value);
+          }
+        }
+      },
+      "Add"
+    )
+  ]);
+
+  return view;
+}
+
+const container = document.getElementById("app");
+
+// function render(oldNode, newNode) {
+//   container.innerHTML = "";
+
+//   patch(toVNode(container), newNode);
+// }
+
+// render(container, template(state));
+
+function render(oldNode, newNode) {
+  container.innerHTML = "";
+
+  patch(toVNode(container), newNode);
+}
+
+render(container, template(state));
